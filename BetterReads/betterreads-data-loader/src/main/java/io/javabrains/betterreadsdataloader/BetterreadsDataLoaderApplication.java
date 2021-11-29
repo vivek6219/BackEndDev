e1036b84b04822f8c9c025a2abaa7262947700cf
V <vivek6219@gmail.com>
2021-11-28 19:58:36 -0800

    New tutorial spring project to learn cassandra and get better understanding of spring

---
+++ BetterReads/betterreads-data-loader/src/main/java/io/javabrains/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -0,0 +1,28 @@
+package io.javabrains.betterreadsdataloader;
+
+import java.nio.file.Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import connection.DataStaxAstraProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
+public class BetterreadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle =  astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+ 
+}
+	
